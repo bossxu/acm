073da22fe6c55e5f537b9a36f05773c77447742c
langman <756753676@qq.com>
2017-10-27 17:15:53 +0800

    11

--- math/组合数学/uva10294.cpp
+++ math/组合数学/uva10294.cpp
@@ -20,14 +20,15 @@ const int mod = 1e9+7 ;
 #define clr(a,x) memset(a,x,sizeof(a))
 #define cle(a,n) for(int i=1;i<=n;i++) a.clear();
 const double eps = 1e-6;
+//置换群的概念
 int gcd(int a,int b)
 {
-  return b?a:gcd(b,a%b);
+  return a%b==0?b:gcd(b,a%b);
 }
 int main()
 {
-  freopen("in.txt","r",stdin);
-  freopen("out.txt","w",stdout);
+  //freopen("in.txt","r",stdin);
+  //freopen("out.txt","w",stdout);
   int a,b;
   ll dp[52];
   while(cin>>a>>b)
@@ -37,6 +38,18 @@ int main()
     {
       dp[i] = dp[i-1] * b;
     }
+    ll ans=0,sum=0;
+    for(int i = 0;i<a;i++)
+    {
+      ans += dp[gcd(i,a)];
+      sum += dp[gcd(i,a)];
+    }
+    if(a%2 == 1)
+    sum += a*dp[(a-1)/2+1];
+    else
+    sum += a/2*(dp[(a/2)]+dp[a/2+1]);
+    cout<<ans/a<<" ";
+    cout<<sum/a/2<<endl;
   }
 
   return 0;

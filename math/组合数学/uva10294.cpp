f8f1b269ab28b7c0a43db55097f71537bf739e2d
langman <756753676@qq.com>
2017-10-27 13:59:02 +0800

    10.26

---
+++ math/组合数学/uva10294.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<vector>
+#include<cmath>
+#include<set>
+#include<cstdlib>
+#include<functional>
+#include<climits>
+#include<cctype>
+#include<iomanip>
+using namespace std;
+typedef long long ll;
+#define INF 0x3f3f3f3f
+const int mod = 1e9+7 ;
+#define clr(a,x) memset(a,x,sizeof(a))
+#define cle(a,n) for(int i=1;i<=n;i++) a.clear();
+const double eps = 1e-6;
+int gcd(int a,int b)
+{
+  return b?a:gcd(b,a%b);
+}
+int main()
+{
+  freopen("in.txt","r",stdin);
+  freopen("out.txt","w",stdout);
+  int a,b;
+  ll dp[52];
+  while(cin>>a>>b)
+  {
+    dp[0] = 1;
+    for(int i = 1;i<=a+1;i++)
+    {
+      dp[i] = dp[i-1] * b;
+    }
+  }
+
+  return 0;
+}

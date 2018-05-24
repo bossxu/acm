1948d1b0a39976da3c55a94105da2d32d4e15c56
langman <756753676@qq.com>
2018-05-24 16:34:29 +0800

    算法

--- 比赛/newcoder/山东/f.cpp
+++ 比赛/newcoder/山东/f.cpp
@@ -25,18 +25,24 @@ struct Node
   ll l,r;
   ll cha;
 }node[5];
+inline ll read()//加快输入输出
+{
+    char k=0;char ls;ls=getchar();for(;ls<'0'||ls>'9';k=ls,ls=getchar());
+    ll x=0;for(;ls>='0'&&ls<='9';ls=getchar())x=x*10+ls-'0';
+    if(k=='-')x=0-x;return x;
+}
 int main()
 {
-  freopen("in.txt","r",stdin);
-  freopen("out.txt","w",stdout);
-  std::ios::sync_with_stdio(false);
+  //freopen("in.txt","r",stdin);
+  //freopen("out.txt","w",stdout);
+  //std::ios::sync_with_stdio(false);
   int t;
   cin>>t;
   while(t--)
   {
     for(int i = 1;i<=4;i++)
     {
-      cin>>node[i].l>>node[i].r;
+      node[i].l = read();node[i].r = read();
       node[i].cha = node[i].r - node[i].l+1;
     }
     ll sum = 1;
@@ -50,31 +56,27 @@ int main()
       ll  r = min(node[i].r,node[j].r);
       ll cha = r-l+1;
       if(cha<0) cha = 0;
-      for(int k = 1;k<=4;k++)
-      {
-        if(k!=i && k!=j)
-        {
-           cha = cha*node[k].cha%mod;
-        }
-      }
+      int p = (i+2)%4;
+      if(p == 0) p =4;
+      cha = cha*node[p].cha%mod;
+       p = (i+3)%4;
+      if(p == 0) p =4;
+      cha = cha*node[p].cha%mod;
+
       a = (cha+a)%mod;
     }
     ll b = 0;
     for(int i = 1;i<=4;i++)
     {
-       int j = (i+1)%4; if(j == 0) j =4;
-       int k = (i+2)%4; if(k == 0) k =4;
+         int j = (i+1)%4; if(j == 0) j =4;
+         int k = (i+2)%4; if(k == 0) k =4;
           ll l = max(node[i].l,max(node[j].l,node[k].l));
-          ll r = min(node[i].r,max(node[j].r,node[k].r));
+          ll r = min(node[i].r,min(node[j].r,node[k].r));
           ll cha = r-l+1;
           if(cha<0) cha = 0;
-          for(int p = 1;p<=4;p++)
-          {
-            if(p!=i && p!=j && p!=k)
-            {
-              cha = cha*node[p].cha%mod;
-            }
-          }
+          int p = (i+3)%4;
+          if(p == 0) p =4;
+          cha = cha*node[p].cha%mod;
           b = (cha+b)%mod;
     }
     //cout<<b<<endl;
@@ -94,13 +96,13 @@ int main()
      r1 = min(node[1].r,node[4].r);
      cha = r-l+1;
      cha1 = r1-l1+1;
-    if(cha<0) cha = 0;
-    if(cha1<0) cha1= 0;
+     if(cha<0) cha = 0;
+     if(cha1<0) cha1= 0;
     //cout<<cha<<" "<<cha1<<endl;
-    b = (b+cha*cha1%mod)%mod;
+     b = (b+cha*cha1%mod)%mod;
   //  cout<<b<<endl;
      l = max(node[1].l,max(node[2].l,max(node[4].l,node[3].l)));
-     r = min(node[1].r,max(node[2].r,max(node[3].r,node[4].r)));
+     r = min(node[1].r,min(node[2].r,min(node[3].r,node[4].r)));
      cha = r-l+1;
 
     if(cha<0) cha=0;

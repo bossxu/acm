d1991a78517c2b6fe9bec63a5f493b202f9b4e00
[langman] <[756753676@qq.com]>
2018-08-30 19:49:41 +0800

    斜率dp

--- dp/优化下的dp/hdu3415.cpp
+++ dp/优化下的dp/hdu3415.cpp
@@ -13,6 +13,48 @@ const double eps = 1e-6;
 const int N = 1e5+5;
 vector<int>q;
 ll sum[N*2];
+ll que[N*2];
+ll dp[N*2];
+int n,k;
+int ans,st,len;
+void slove()
+{
+  for(int i = 1;i<=n;i++)
+  {
+    q.pb(q[i]);
+  }
+  sum[0] = 0;
+  for(int i = 1;i<=2*n;i++)
+  {
+    sum[i] = sum[i-1] +q[i];
+  }
+  int head = 1,tail = 1;
+  ans = sum[1],st = 1,len = 1;
+  for(int i = 1;i<=2*n;i++)
+  {
+    while(head!=tail && (i-que[head]>k || sum[que[head]]<=sum[que[head+1]])) head++;
+    dp[i] = sum[i] - sum[que[head]];
+    if(dp[i] >= ans)
+    {
+      if(ans == dp[i])
+      {
+        if(i-que[head] < len-1)
+      {
+        st = que[head]+1;
+        len = i-que[head];
+      }
+      }
+      else
+      {
+        st = que[head]+1;
+        len = i-que[head];
+      }
+      ans = dp[i];
+    }
+    while(head!=tail && sum[que[tail]] <= sum[i] ) tail--;
+    que[++tail] = i;
+  }
+}
 int main()
 {
   ios_close;
@@ -20,25 +62,17 @@ int main()
   cin>>t;
   while(t--)
   {
-    int n,k;
     cin>>n>>k;
     q.clear();
+    q.pb(-1);
     int a;
     for(int i = 1;i<=n;i++)
     {
       cin>>a;
       q.pb(a);
     }
-    for(int i = 0;i<n;i++)
-    {
-      q.pb(q[i]);
-    }
-    sum[0] = q[0];
-    for(int i = 1;i<2*n;i++)
-    {
-      sum[i] = sum[i-1] +q[i];
-    }
-
+    slove();
+    cout<<ans<<" "<<st<<" "<<len<<endl;
   }
   return 0;
 }
